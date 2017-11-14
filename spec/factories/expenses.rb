420674e48085d57dea4e063155c99437a569f7d4
Nicolas & Nailson <ncs.cr4sh@gmail.com>
2017-11-14 16:01:35 -0200

    Invite cant be accepted afeter paydate
    This commit depends on this push
     * https://github.com/gleisernicolas/rateio/tree/payment_cannot_be_made_after_pay_Date

--- spec/factories/expenses.rb
+++ spec/factories/expenses.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :expense do
     title 'Futebol de domingo'
     description 'Futebol com o Christian'
-    event_date '17/12/2017'
-    pay_date '10/12/2017'
+    event_date 30.days.from_now
+    pay_date 37.days.from_now
     total_price '200'
     participants_amount '10'
   end
