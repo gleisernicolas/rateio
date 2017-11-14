3a09977933ba7014d945675e6d24442bb6ba38ac
Nicolas & Nailson <ncs.cr4sh@gmail.com>
2017-11-14 15:17:03 -0200

    Payment can't be done after pay date

--- app/controllers/user_expenses_controller.rb
+++ app/controllers/user_expenses_controller.rb
@@ -1,19 +1,16 @@
 class UserExpensesController < ApplicationController
   before_action :authenticate_user!, only: [:update, :paid, :show]
-
   before_action :set_user_expense, only: [:update, :show, :paid, :voucher]
+  before_action :validate_payment, only: [:voucher]
 
   def voucher
-    if !@user_expense.expense.payment_is_available?
-      flash[:alert] = t('user_expense.voucher.date_limit_exceded')
-    else
-      @user_expense.update(voucher_params)
-      flash[:notice] = if @user_expense.mark_as_pending
-                         t('user_expense.voucher.success')
-                       else
-                         t('user_expense.voucher.error')
-                       end
-    end
+    @user_expense.update(voucher_params)
+    flash[:notice] = if @user_expense.mark_as_pending
+                       t('user_expense.voucher.success')
+                     else
+                       t('user_expense.voucher.error')
+                     end
+
     redirect_to expense_path(@user_expense.expense)
   end
 
@@ -34,5 +31,14 @@ class UserExpensesController < ApplicationController
 
   def voucher_params
     params.require(:user_expense).permit(:payment_voucher)
+  end
+
+  def validate_payment
+    set_user_expense
+
+    unless @user_expense.expense.payment_is_available?
+      flash[:alert] = t('user_expense.voucher.date_limit_exceded')
+      redirect_to expense_path(@user_expense.expense)
+    end
   end
 end
